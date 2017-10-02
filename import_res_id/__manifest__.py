25294d5105727388de297132ea90cb982dfc23e1
Thibault Francois <tfr@openerp.com>
2017-10-02 12:03:11 +0200

    [ADD] Import res_id

---
+++ import_res_id/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Import res_id with external id',
+    'description' : "",
+    'version': '0.1',
+    "category": 'Extra Tools',
+    'author': 'Thibault Francois',
+    'depends': ['mail'],
+    'website': 'http://www.openerp.com',
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'active': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
